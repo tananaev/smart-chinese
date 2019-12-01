b130b6a5f32ae9647cef3916de7f635f84f690d4
Anton Tananaev <anton.tananaev@gmail.com>
2019-11-30 23:18:07 -0800

    Add next button

--- app/src/main/java/com/tananaev/language/MainActivity.kt
+++ app/src/main/java/com/tananaev/language/MainActivity.kt
@@ -2,18 +2,22 @@ package com.tananaev.language
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var data: List<Word>
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        data = DataLoader.load(this, R.raw.hsk1)
+        val data = DataLoader.load(this, R.raw.hsk1)
+
+        findViewById<View>(R.id.button_next).setOnClickListener {
+            updateView(data[Random.nextInt(0, data.size)])
+        }
+
         updateView(data[Random.nextInt(0, data.size)])
     }
 
